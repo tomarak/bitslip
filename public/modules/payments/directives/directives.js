7cca587d1f197ff5ed177efcf40500dac6b22138
Lauren <lhspiegel@gmail.com>
2015-05-05 17:01:09 -0700

    Removes console.logs

--- public/modules/payments/directives/directives.js
+++ public/modules/payments/directives/directives.js
@@ -2,10 +2,6 @@ var directives = angular.module('directives');
 
 directives.directive('autocomplete', ['$http', function($http) {
     return function (scope, element, attrs) {
-        console.log("element", element);
-        console.log("attrs", attrs);
-        console.log(scope, "scope");
-
         $(function(){
             $(element).autocomplete({
             minLength:3,
@@ -13,8 +9,6 @@ directives.directive('autocomplete', ['$http', function($http) {
                 var url = "/search_user/?term=" + request.term;
                 $http.get(url).success( function(data) {
                     var list = data[0].username;
-                    console.log("list", list)
-                    console.log("response", response)
                     response(data);
                 });
             },
@@ -23,9 +17,7 @@ directives.directive('autocomplete', ['$http', function($http) {
                 return false;
             },
             select:function (event, ui) {
-                console.log(scope, "scopere")
                 scope.recipient = ui.item.username;
-                console.log("ui", ui.item);
                 scope.$apply();
                 return false;
             },
